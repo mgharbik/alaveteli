1f45c5e552b87c3f04574b4dfd297728b73c897d
Graeme Porteous <graeme@rgbp.co.uk>
2017-12-19 12:57:13 +0000

    Record who last updated an announcement

--- app/controllers/admin_announcements_controller.rb
+++ app/controllers/admin_announcements_controller.rb
@@ -46,7 +46,9 @@ class AdminAnnouncementsController < AdminController
   def announcement_params
     if announcement_params = params[:announcement]
       keys = { translated_keys: [:locale, :title, :content] }
-      translatable_params(keys, announcement_params)
+      translatable_params(keys, announcement_params).merge(
+        user: current_user
+      )
     else
       {}
     end
